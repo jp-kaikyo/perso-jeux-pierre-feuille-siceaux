34e7ae269a43271b0503a8106e05915cd3eeecc8
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-07-29 05:50:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//// variable ////
+
+
+// info sur joueur ia //
+
+let score_joueur1 = 0
+let choix_joueur1 = ""
+
+let score_joueur2 = 0
+let choix_joueur2 = ""
+
+let score_ia = 0
+let choix_ai = ""
+
+// info sur le lencement de la partie //
+
+let game = ""
+
+
+
+
+//// fonction ////
+
+
