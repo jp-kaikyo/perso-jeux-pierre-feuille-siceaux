b89b6a8ac37a9e075b2aba30822a712b2333daf5
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-08-02 05:56:04 +0200

    mise a niveau du code

--- script.js
+++ script.js
@@ -9,16 +9,13 @@ let choix_joueur1 = ""
 let score_joueur2 = 0
 let choix_joueur2 = ""
 
-let score_ia = 0
-let choix_ai = ""
+let score_iabot = 0
+let choix_aibot = ""
+
 
 // info sur le lencement de la partie //
 
 let game = ""
 
 
-
-
 //// fonction ////
-
-
